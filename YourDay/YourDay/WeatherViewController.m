5c0a4ac9752599e28c3b2dad977ae19eccea00da
Michael Hoffman <mhoffman999@gmail.com>
2015-03-30 23:09:41 -0400

    Added labels to Storyboard WeatherVC

--- YourDay/YourDay/WeatherViewController.m
+++ YourDay/YourDay/WeatherViewController.m
@@ -11,8 +11,24 @@
 @interface WeatherViewController ()
 
 @property (weak, nonatomic) IBOutlet UIImageView *backgroundView;
+
 @property (weak, nonatomic) IBOutlet UIView *mainDesignContainer;
 @property (weak, nonatomic) IBOutlet UIImageView *mainDesign;
+
+//Containers 1, 2, and 3 reside within the mainDesignContainer
+@property (weak, nonatomic) IBOutlet UIView *container1;
+@property (weak, nonatomic) IBOutlet UIImageView *currentWeatherIcon;
+@property (weak, nonatomic) IBOutlet UILabel *hiTempLabel;
+
+@property (weak, nonatomic) IBOutlet UIView *container2;
+@property (weak, nonatomic) IBOutlet UILabel *currentTempLabel;
+@property (weak, nonatomic) IBOutlet UILabel *weatherDescriptionLabel;
+
+@property (weak, nonatomic) IBOutlet UIView *container3;
+@property (weak, nonatomic) IBOutlet UILabel *loTempLabel;
+@property (weak, nonatomic) IBOutlet UILabel *feelsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *feelsLikeTempLabel;
+
 @property (weak, nonatomic) IBOutlet UIView *scrollContainer;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIView *scrollContent;
@@ -34,9 +50,20 @@
     [self.navigationController.navigationBar setBarTintColor:[UIColor whiteColor]];
     self.navigationController.navigationBar.translucent = NO;
     
-    self.scrollContainer.backgroundColor = [UIColor clearColor];
-    self.scrollContent.backgroundColor = [UIColor clearColor];
     self.scrollView.showsHorizontalScrollIndicator = NO;
+    
+    //Make as method: "containersClearColor"
+    for (UIView *containers in [self containersArray])
+    {
+        containers.backgroundColor = [UIColor clearColor];
+    }
+    
+    //Make as method: "weatherPageBlue"
+    UIColor *navyFog = [UIColor colorWithRed:61.0/255 green:73.0/255 blue:96.0/255 alpha:1.0];
+    
+    self.currentTempLabel.textColor = navyFog;
+    self.feelsLabel.textColor = navyFog;
+    self.feelsLikeTempLabel.textColor = navyFog;
     
     //Creates TOP and BOTTOM borders for scrollContainer
     CGFloat border = 1;
@@ -104,6 +131,12 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSArray *)containersArray
+{
+    NSArray *containerViews = @[self.mainDesignContainer, self.container1, self.container2, self.container3, self.scrollContainer, self.scrollContent];
+    return containerViews;
 }
 
 /*
